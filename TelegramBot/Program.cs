79951cc14af93cd308b68e9d91e9e7d6911c0cc7
Sherali <83146168+Uzbektigers2001@users.noreply.github.com>
2022-07-06 10:44:16 +0500

    Bacground servise Added to Servises as Hosted

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,8 +1,10 @@
 using Telegram.Bot;
+using TelegramBot.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton(new TelegramBotClient(builder.Configuration.GetValue("BotToken", string.Empty)));
+builder.Services.AddHostedService<BackgroundServices>();
 
 var app = builder.Build();
 
